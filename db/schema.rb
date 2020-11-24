f6b04904ccd4c54d8726315b9d72dbc63146cedf
adrien goux <adriengoux@gmail.com>
2020-11-24 15:24:18 +0100

    add name to trip model - migration required

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_123625) do
+ActiveRecord::Schema.define(version: 2020_11_24_140907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_123625) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 

7f6fb439e46051ca70bb6861d3374830a85489eb
yuva k <yuvarajan1989@gmail.com>
2020-11-25 13:19:39 +0800

    created seeds for trip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+require "date"
+require "open-uri"
+
+puts "Deleting databases"
+Trip.destroy_all
+User.destroy_all
+
+puts "Creating users"
+owner1 = User.new(
+  email: "owner1@gmail.com",
+  password: '111111'
+)
+owner1.save
+
+owner2 = User.new(
+  email: "owner2@gmail.com",
+  password: '111111'
+)
+owner2.save
+
+puts "Creating trip"
+
+
+10.times do 
+  start_date = Date.today + rand(10..100)
+  end_date = start_date + 5
+  name = Faker::Address.country
+  
+  user_id_array = []
+  User.all.each do |user|
+    user_id_array << user.id
+  end
+
+  trip = Trip.new(
+    name: name + ' trip',
+    location: name,
+    start_date: start_date,
+    user_id: user_id_array.sample,
+    end_date: end_date 
+  )
+  trip.save
+
+end
+
+puts "Trips created"
+
+
+
+
+

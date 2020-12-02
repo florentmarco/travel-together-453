d0510950151cc67c9ce6d90b90ee91b3a2d044be
yuvarajan k <yuvarajan1989@gmail.com>
2020-12-02 17:55:29 +0800

    enable datepickr

--- db/schema.rb
+++ db/schema.rb
@@ -15,21 +15,12 @@ ActiveRecord::Schema.define(version: 2020_11_28_062444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "chatrooms", force: :cascade do |t|
-    t.bigint "trip_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["trip_id"], name: "index_chatrooms_on_trip_id"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "chatroom_id", null: false
-    t.index ["chatroom_id"], name: "index_comments_on_chatroom_id"
     t.index ["item_id"], name: "index_comments_on_item_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -111,7 +102,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_062444) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -125,8 +116,6 @@ ActiveRecord::Schema.define(version: 2020_11_28_062444) do
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
-  add_foreign_key "chatrooms", "trips"
-  add_foreign_key "comments", "chatrooms"
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
   add_foreign_key "guests", "trips"

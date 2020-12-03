5b7ae60484c504fc178dac0e13faf0d33ee1b35c
Summer S <summer.s2020@icloud.com>
2020-12-03 23:50:38 +0900

    fixed bug

--- app/javascript/channels/trip_channel.js
+++ app/javascript/channels/trip_channel.js
@@ -2,14 +2,14 @@ import consumer from "./consumer";
 
 const initTripCable = () => {
   const messagesContainer = document.getElementById('messages');
-  // console.log("file imported?")
+  console.log("file imported?");
   if (messagesContainer) {
     const id = messagesContainer.dataset.tripId;
 
     consumer.subscriptions.create({ channel: "TripChannel", id: id }, {
       received(data) {
         // console.log(data); // called when data is broadcast in the cable
-        messagesContainer.insertAdjacentHTML('beforeend', data);
+        messagesContainer.insertAdjacentHTML('afterbegin', data);
       },
     });
   }

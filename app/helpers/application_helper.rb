e463136a9a0c4f231deea2e3b85ee9d8681828f0
florentmarco <florent.jean.marco@gmail.com>
2020-12-15 22:18:18 +0800

    dynamic button integrated, needs refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def path_for_route(path_info, trip=nil)
+    case
+  when path_info.match?(/trips\/\d+/)
+      new_trip_item_path(trip)
+    else
+      new_trip_path
+    end
+  end
 end

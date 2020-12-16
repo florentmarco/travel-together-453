17c0a7a9eaf8dbad1eb56eea6587081e748a3758
Du Guoxin <anton.dgx@gmail.com>
2020-12-16 10:48:41 +0800

    adding unsplash

--- db/testing.rb
+++ db/testing.rb
@@ -1,7 +1,15 @@
-require "open-uri"
-require 'json'
+# require "open-uri"
+# require 'json'
 
-url = 'https://randomuser.me/api/'
-user_serialized = open(url).read
-user = JSON.parse(user_serialized)
-puts "#{user['results'].first["picture"]["large"]}"
+# url = 'https://randomuser.me/api/'
+# user_serialized = open(url).read
+# user = JSON.parse(user_serialized)
+# puts "#{user['results'].first["picture"]["large"]}"
+
+require "unsplash"
+
+# photo = Unsplash::Photo.find("tAKXap853rY")
+
+# search_results = Unsplash::Photo.search("cats")
+photo = Unsplash::Photo.find("tAKXap853rY")
+p photo

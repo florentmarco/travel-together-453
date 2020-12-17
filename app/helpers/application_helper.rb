65443efc8b7c86c9844dfa289c17b5fbeccb85a7
Du Guoxin <anton.dgx@gmail.com>
2020-12-17 17:50:32 +0800

    refactor unsplash api helper to get pics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,9 @@
 module ApplicationHelper
   # unsplash api
-  def get_trip_pic(trip, api_key)
-    url = "https://api.unsplash.com/search/photos?page=1&query=#{trip}&client_id=#{api_key}"
-    user_serialized = open(url).read
-    user = JSON.parse(user_serialized)
-    user["results"].first["urls"]["raw"]
-  end
-
-  def get_accommodation_pic(accommodation, api_key)
-    url = "https://api.unsplash.com/search/photos?page=1&query=#{accommodation}&client_id=#{api_key}"
-    user_serialized = open(url).read
-    user = JSON.parse(user_serialized)
-    user["results"].first["urls"]["raw"]
-  end
-
-  def get_activity_pic(activity, api_key)
-    url = "https://api.unsplash.com/search/photos?page=1&query=#{activity}&client_id=#{api_key}"
-    user_serialized = open(url).read
-    user = JSON.parse(user_serialized)
-    user["results"].first["urls"]["raw"]
+  def get_query_pic(item_name, api_key)
+    url = "https://api.unsplash.com/search/photos?page=1&query=#{item_name}&client_id=#{api_key}"
+    item_serialized = open(url).read
+    item = JSON.parse(item_serialized)
+    item["results"].first["urls"]["thumb"]
   end
 end
